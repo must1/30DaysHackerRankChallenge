fc7f6af22ba301981b403f1786b93bc0de0871ba
must1 <must.janik@gmail.com>
2018-01-28 22:42:55 +0100

    Day14 of solving hackerrank

---
+++ Day14/Solution.java
@@ -0,0 +1,54 @@
+package Day14;
+
+import java.util.Scanner;
+import java.math.*;
+
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+    public Difference(int[] elements) {
+        this.elements = elements;
+    }
+
+    public void computeDifference() {
+        int min = elements[0];
+        int max = elements[0];
+
+        for (int i = 0; i < elements.length; i++) {
+
+            if (min > elements[i])
+                min = elements[i];
+            else
+                min = min;
+            if (max < elements[i])
+                max = elements[i];
+            else
+                max = max;
+            maximumDifference = Math.abs(max - min);
+        }
+
+    }
+}
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
+
